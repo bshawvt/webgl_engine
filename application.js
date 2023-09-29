4e5dc9b43883a6af0e8476f5179b95ea5e2a4a38
Sasakwa-PC <bshawvt@gmail.com>
2023-09-29 14:46:47 -0400

    untracked

--- application.js
+++ application.js
@@ -4,21 +4,37 @@
 		var self = this;
 		this.loader = new Loader(loader);
 		this.animator = null;
-		// load additional files
-		this.loader.load(["lib/animator.js"], function(loader) {
-			var {Animator} = loader.import;
-			self.animator = new Animator(self.update, self.render);
-			self.animator.animate();
-			self.animator.showOverlay();
+		this.graphics = null;
+
+		this.loader.load(["lib/animator.js", "lib/graphics.js"], function(loader) {
+			self.init();
 		});
 	};
 	
+	MainApplication.prototype.init = function() {
+		var self = this;
+		var {Animator, Graphics} = this.loader.import;
+		this.graphics = new Graphics(this);
+		this.animator = new Animator(function(animator, elapsed) {
+			self.update(animator, elapsed);
+		}, function(animator, elapsed) {
+			self.render(animator, elapsed);
+		});
+		// todo: this overlay is messy
+		this.animator.showOverlay();
+
+		// webgl setup
+		
+		// begin
+		this.animator.animate();
+	};
+	
 	MainApplication.prototype.update = function(context, elapsed) {
-		//console.log("step...", context, dt);
+		//
 	};
 
 	MainApplication.prototype.render = function(context, elapsed) {
-		//console.log("render...", context, dt);
+		this.graphics.clear();
 	};
 
 	window.export = {
--- documentation/documentation.js
+++ documentation/documentation.js
@@ -3,8 +3,8 @@
 */
 window.docs = Object.create({
 	//
-	"init.js": "Document entry point template that loads a minimum set of files.\n\nThe variable `sources` (init.js:2) is an Array of URIs \
-		that will be loaded before the template application starts.\n \
+	"init.js": "Document entry point template that loads a minimum set of files.\n\nThe variable `sources` \
+		(init.js:2) is an Array of URIs that will be loaded before the template application starts.\n \
 		After all resources are loaded this script will create a new instance of MainApplication (application.js).",
 
 	"application.js": "A template that tries to use all pieces of this project together.\n\n \
@@ -14,34 +14,62 @@ window.docs = Object.create({
 		at a fixed constant rate while executing `Animator.render` as quickly as possible.\n\n \
 		Render and update methods are user defined and must be set either through the Animator constructor \
 		or through `Animator.onRender` & `Animator.onUpdate` methods.\n\n \
+		\
 		Example usage:\n \
 		`var anim = new Animator(fnRender, fnUpdate);\n \
 		anim.animate();`\n\n \
 		\
-		Constructor::Animator(fnUpdate => Function, fnRender => Function):\n \
+		Constructor::Animator(`fnUpdate?` => Function, `fnRender?` => Function):\n \
 		\xa0\xa0\xa0\xa0 Both params are optional and overwrite their respective prototypes.\n\n \
 		\
-		Animator.animate(elapsed => Float):\n \
+		Animator.animate(`elapsed` => Float):\n \
 		\xa0\xa0\xa0\xa0 A non-blocking loop that executes update and render methods. Returns undefined.\n\n \
 		\
-		Animator.onUpdate(fnUpdate => Function):\n \
+		Animator.onUpdate(`fnUpdate` => Function):\n \
 		\xa0\xa0\xa0\xa0 Set an application defined update function. Returns undefined.\n \
-		\xa0\xa0\xa0\xa0 fnUpdate(animator => Animator, now => Float): Executed at a fixed \
+		\xa0\xa0\xa0\xa0 fnUpdate(`animator` => Animator, `now` => Float): Executed at a fixed \
 		rate before render.\n\n \
 		\
-		Animator.onRender(fnRender => Function):\n \
+		Animator.onRender(`fnRender` => Function):\n \
 		\xa0\xa0\xa0\xa0 Set an application defined render function. Returns undefined.\n \
-		\xa0\xa0\xa0\xa0 fnRender(animator => Animator, now => Float): Executed as quickly as \
+		\xa0\xa0\xa0\xa0 fnRender(`animator` => Animator, `now` => Float): Executed as quickly as \
 		possible after render.\n\n \
 		\
 		Animator.getMetrics():\n \
 		\xa0\xa0\xa0\xa0 Returns an object full of metrics.\n\n",
+
+	"lib/graphics.js": "A graphics class that creates and manages a WebGL context. \
+		Provides methods for drawing to and manipulating the new context.\n\n \
+		\
+		Example usage:\n \
+		`var g = new Graphics();\n \
+		g.clear();`\n\n \
+		\
+		Constructor::Graphics(`opts?` => Object):\n \
+		\xa0\xa0\xa0\xa0 The optional `opts` param is used to specify the context's initial states \
+		such as glClear color. The `opts` object supports the following properties:\n \
+		`{`\n \
+		`\xa0\xa0\xa0\xa0 useGl2` => Boolean: Create a WebGl2 context.\n \
+		`\xa0\xa0\xa0\xa0 ` => Boolean: Create a WebGl2 context.\n \
+		`}`\n \
+		\
+		\n\n \
+		\
+		Graphics.setClearColor(`rgba` => String):\n \
+		Graphics.setClearColor(`red` => Unsigned Byte, `green` => Unsigned Byte, `blue` => Unsigned Byte, `alpha` => Unsigned Byte):\n \
+		\xa0\xa0\xa0\xa0 Set the clear color used by glClear. If the type of the first param is \
+		a String then the `0xRRGGBB`, `0xRRGGBBAA` notation is expected. Returns undefined.\n\n \
+		\
+		\
+		\
+		",
 
 	"lib/loader.js": "A class that dynamically imports scripts and media into the active document.\n\n\
 		Images, audio, json, and other binary files are imported as Resource objects. \
 		Resources can be retrieved with the `Loader.get` method.\n \
 		Loaded scripts can export objects, classes, functions, etc through the global window object \
 		and imported later through `Loader.import`. Eg, `window.export = {MyClass, MyOtherClass}`.\n\n \
+		\
 		Example usage:\n \
 		`new Loader().load('myclass.js', 'path/item.png', function(loader) { \n\
 		\xa0\xa0\xa0\xa0 var {MyClass, MyItems} = loader.import; // import classes\n \
@@ -49,17 +77,17 @@ window.docs = Object.create({
 		\xa0\xa0\xa0\xa0 console.log(loader.get('item.png'));\n \
 		});`\n\n \
 		\
-		Constructor::Loader(loader => Loader):\n \
+		Constructor::Loader(`loader?` => Loader):\n \
 		\xa0\xa0\xa0\xa0 If the optional `loader` param is defined then any previous resources \
 		will be appended to the new Loader instance.\n\n \
 		\
-		Loader.load(src => Array, fnOnDone => Function, fnOnLoad => Function, fnOnError => Function):\n \
+		Loader.load(`src` => Array, `fnOnDone?` => Function, `fnOnLoad?` => Function, `fnOnError?` => Function):\n \
 		\xa0\xa0\xa0\xa0 Loads a list of URI references and executes callbacks when appropriate. Returns undefined.\n \
-		\xa0\xa0\xa0\xa0 fnOnDone(loader => Loader): Executed after all URI references have had a chance to load.\n \
-		\xa0\xa0\xa0\xa0 fnOnLoad(type => String, name => String): Executed after a URI reference has loaded.\n \
-		\xa0\xa0\xa0\xa0 fnOnError(url => String): Executed when a URI reference fails to load.\n\n \
+		\xa0\xa0\xa0\xa0 fnOnDone(`loader` => Loader): Optional. Executed after all URI references have had a chance to load.\n \
+		\xa0\xa0\xa0\xa0 fnOnLoad(`type` => String, `name` => String): Optional. Executed after a URI reference has loaded.\n \
+		\xa0\xa0\xa0\xa0 fnOnError(`url` => String): Optional. Executed when a URI reference fails to load.\n\n \
 		\
-		Loader.get(filename => String):\n \
+		Loader.get(`filename` => String):\n \
 		\xa0\xa0\xa0\xa0 Expects filename to be trimmed. Eg, `filename.png` instead of `path/filename.png`. Returns Resource or null.\n\n \
 		A Resource object is defined as: `{ filename: String, data: Object }`\n\n"
 });
